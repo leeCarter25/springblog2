ba3d574083c1f4fd64f068f4235e12e6fdab9aea
Christian Carter <carter.lee2028@gmail.com>
2022-11-18 15:32:55 -0600

    spring views exercise

--- src/main/java/com/example/springblog2/HomeController.java
+++ src/main/java/com/example/springblog2/HomeController.java
@@ -1,9 +1,13 @@
 package com.example.springblog2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -13,4 +17,28 @@ public class HomeController {
     public String landing() {
         return "This is the landing page!";
     }
-}
+
+        // To show anchor tag options page
+        @GetMapping("/roll-dice")
+        public String showChoices() {
+            return "roll-dice";
+        }
+
+        // To show results of guess
+        @GetMapping("/roll-dice/{n}")
+        public String showResults(@PathVariable int n, Model model) {
+            // Establish the random number variable
+            int randomNum = (int) Math.floor(Math.random() * ((7 - 1) + 1));
+
+            // check if the path variable is equal to the random number
+
+            boolean result = n == randomNum;
+
+            model.addAttribute("result", result);
+            model.addAttribute("randomNum", randomNum);
+
+            return "roll-dice";
+        }
+
+
+    }
--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,4 +1,4 @@
-package com.example.springblog2;
+package models;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
